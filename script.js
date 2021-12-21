18fa1a0fc465c0fae346968d69b3bee0369df009
Illensky <alexis.laroche.02240@gmail.com>
2021-12-21 11:15:23 +0100

    Project done

--- script.js
+++ script.js
@@ -1,3 +1,49 @@
-const holes = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "hole";
-const scoreBoard = //utiliser la méthode jquery appropriée pour selectionner l'élément ayant pour classe "score";
-const moles = // utilier la méthode jquery appropriée pour selectionner les éléments ayant pour classe "mole";
+const holes = $('.hole')
+const scoreBoard = $(".score")
+const moles = $('.mole')
+
+
+let count = 0;
+let time = 0;
+
+function getTime() {
+    let time = Math.round(Math.random() * 1000)
+    if (time < 200) {
+        return getTime();
+    }
+    return time
+}
+
+
+$('#startGame').click(function test () {
+    scoreBoard.text(count.toString())
+    timeout = setTimeout( function () {
+
+        const randomHole = Math.trunc(Math.random() * holes.length)
+        holes.eq(randomHole).addClass("up")
+
+
+        holes.eq(randomHole).click(function () {
+            holes.eq(randomHole).removeClass("up")
+            count++
+            scoreBoard.text(count.toString())
+            holes.eq(randomHole).off("click")
+        })
+
+        setTimeout(() => {
+            holes.eq(randomHole).removeClass("up")
+            holes.eq(randomHole).off("click")
+        }, getTime())
+
+        time += 0.5
+
+        if (time < 10) {
+            test();
+        }
+        else {
+            time = 0;
+            count = 0;
+        }
+    }, 500)
+
+})
